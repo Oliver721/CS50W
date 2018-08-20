a6eb8306baf4f48990a20f04a6b2bcdc76ea65b9
cd28 <wuchengdong628@gmail.com>
2018-08-20 22:16:06 +0900

    update src2

---
+++ src/src2/补充代码/variables1/application.py
@@ -0,0 +1,12 @@
+import random
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    headline = random.choice(["Hello, world!", "Hi there!", "Good morning!"])
+    return render_template("index.html", headline=headline)
+
+app.run()
